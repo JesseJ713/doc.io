4479fbaa25ed48c598f7f8d165ee47bd1fb71870
JCJ <jesse.jackson.c@gmail.com>
2021-10-26 03:12:43 -0500

    Created an expanded toolbar

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -3,13 +3,25 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 
 export default function TextEditor() {
+  const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['bold', 'italic', 'underline'],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ align: [] }],
+    ['image', 'blockquote', 'code-block'],
+    ['clean'],
+  ];
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
 
     wrapper.innerHTML = '';
     const editor = document.createElement('div');
     wrapper.append(editor);
-    new Quill(editor, { theme: 'snow' });
+    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
   }, []);
 
   return <div className='container' ref={wrapperRef}></div>;
